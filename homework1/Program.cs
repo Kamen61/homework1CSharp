9e8b2756171493edf7ac8c84543ef80f19907f58
Kamen <canen321@gmail.com>
2022-06-23 14:58:32 +0300

    task4

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -49,14 +49,27 @@
 //-3 -> нет
 //7 -> нет
 
+//Console.Write("Введите число : ");
+//int numberA = Convert.ToInt32(Console.ReadLine());
+//int div=numberA % 2;
+//if (div==0)
+//{
+//    Console.Write(numberA + " Число четное");
+//}
+//else
+//{
+//    Console.Write(numberA + " Число не четное");
+//}
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
 Console.Write("Введите число : ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-int div=numberA % 2;
-if (div==0)
+int N = Convert.ToInt32(Console.ReadLine());
+int integer=0;
+while (integer<(N-2))
 {
-    Console.Write(numberA + " Число четное");
-}
-else
-{
-    Console.Write(numberA + " Число не четное");
+    integer=integer+2;
+    Console.Write(integer + ",");
 }
