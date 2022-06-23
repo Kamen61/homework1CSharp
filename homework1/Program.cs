d00d14cb4959cae814e76e0aa94417d4e5fe86a7
Kamen <canen321@gmail.com>
2022-06-23 14:10:10 +0300

    task3

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -25,21 +25,38 @@
 //22 3 9 -> 22
 
 
-Console.Write("Введите первое число : ");
+//Console.Write("Введите первое число : ");
+//int numberA = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите второе число : ");
+//int numberB = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите третье число : ");
+//int numberC = Convert.ToInt32(Console.ReadLine());
+//if (numberA>numberB && numberA>numberC)
+//{
+//    Console.WriteLine(numberA + " Это число самое большое из трех");
+//}
+//if (numberB>numberA && numberB>numberC)
+//{
+//    Console.WriteLine(numberB + " Это число самое большое из трех");
+//}
+//if (numberC>numberA && numberC>numberB)
+//{
+//    Console.WriteLine(numberC + " Это число самое большое из трех");
+//}
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.Write("Введите число : ");
 int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число : ");
-int numberB = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите третье число : ");
-int numberC = Convert.ToInt32(Console.ReadLine());
-if (numberA>numberB && numberA>numberC)
+int div=numberA % 2;
+if (div==0)
 {
-    Console.WriteLine(numberA);
+    Console.Write(numberA + " Число четное");
 }
-if (numberB>numberA && numberB>numberC)
+else
 {
-    Console.WriteLine(numberB);
-}
-if (numberC>numberA && numberC>numberB)
-{
-    Console.WriteLine(numberC);
+    Console.Write(numberA + " Число не четное");
 }
