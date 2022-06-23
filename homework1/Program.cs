d74d040a24c41eebc63d524d88719b58d86c86fa
Kamen <canen321@gmail.com>
2022-06-23 13:54:36 +0300

    task2

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -4,15 +4,42 @@
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
 
+//Console.Write("Введите первое число : ");
+//int numberA = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите второе число : ");
+//int numberB = Convert.ToInt32(Console.ReadLine());
+//if (numberA>numberB)
+//{
+//    Console.WriteLine( numberA + " Это число больше" );
+//}
+//else
+//{
+//    Console.WriteLine(numberB + " Это число больше");
+//}
+
+
+//Задача 2
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+
 Console.Write("Введите первое число : ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число : ");
 int numberB = Convert.ToInt32(Console.ReadLine());
-if (numberA>numberB)
+Console.Write("Введите третье число : ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if (numberA>numberB && numberA>numberC)
 {
-    Console.WriteLine( numberA + " Это число больше" );
+    Console.WriteLine(numberA);
 }
-else
+if (numberB>numberA && numberB>numberC)
 {
-    Console.WriteLine(numberB + " Это число больше");
+    Console.WriteLine(numberB);
+}
+if (numberC>numberA && numberC>numberB)
+{
+    Console.WriteLine(numberC);
 }
